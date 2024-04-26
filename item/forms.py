5a50f7132a53bdf25f653c544181b26b66ec9d41
Berkay Aktunç <berkay.aktunc@gmail.com>
2024-04-26 21:45:04 +0300

    delete & edit added

--- item/forms.py
+++ item/forms.py
@@ -32,3 +32,31 @@ class NewItemForm(forms.ModelForm):
                 'class': INPUT_CLASSES
             })
 		} 
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['category','name', 'description', 'price', 'image','is_sold']
+        widgets = {
+           'name': forms.TextInput(attrs={
+                'placeholder':'Name of the item...',  
+                'class':INPUT_CLASSES
+            }),  
+            'description': forms.Textarea(attrs={
+            	'rows':'5',  
+            	'cols':'10', 
+            	'placeholder':'Description of the item...',   
+            	'class': INPUT_CLASSES
+            }),
+            'price': forms.NumberInput(attrs={
+            	'type':'number', 
+            	'min':'1', 
+            	'step':'any', 
+            	'placeholder':'Price...',    
+            	'class':INPUT_CLASSES
+            }),
+            'image':forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
+		} 
+
